e27e964509700f3c2d7cddbe3ead5ac84b887a06
Arnaud Christ <arnaud.christ@gmail.com>
2019-04-07 16:17:42 +0200

    Update copyright.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: "https://lumen.netlify.com",
   title: "A Blog by Arnaud Christ",
   subtitle: "Personal Blog, mostly technical posts about JavaScript.",
-  copyright: "©2019 All rights reserved.",
+  copyright: "©2019 All rights reser... Who cares ?",
   disqusShortname: "",
   postsPerPage: 4,
   googleAnalyticsId: "UA-73379983-2",
